46ad7fd7bdacdc2855d0ac1fd402314c8fbb1426
Marco T O Valeriano <marco.valeriano@bancointer.com.br>
2023-11-30 15:23:11 -0300

    fix chat gpt

--- apps/web/src/app/flash/page.tsx
+++ apps/web/src/app/flash/page.tsx
@@ -1,17 +1,95 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
+import { openai } from '../../openai'
 
 // {String(flashCardsCount).padStart(2, '0')} -> Maneira para determinar que precisa ter 2 strings e que se não tiver, é para completar com o 0
 
 const flashCardsTotal = 12
 
+
 export default function Flash() {
   // State para "contar" quantos flashcards já foram respondidos
   const [flashCardsCount, setFlashCardCount] = useState(0)
-
   const [shouldShowAnswer, setShouldShowAnswer] = useState(false)
+  const [question, setQuestion] = useState('')
+  const [options, setOptions] = useState([])
+  const [answer, setAnswer] = useState('')
+
+  const fetchQuestion = async () => {
+    const newMessage = {
+      message: `
+         Me faça uma pergunta sobre GOLang avançado e me dê 4 alternativas sobre, além disso me de a resposta correta. Me responda diretamente e com a pergunta e respostas separadas por ponto e virgula. Siga exatamente o modelo abaixo:
+         
+         Pergunta: {coloque a pergunta aqui}
+         Alternativas: {a) alternativa a;b)  alternativa b;c) alternativa c;d) alternativa d}
+         Resposta correta: {coloque a resposta correta aqui}
+         
+        `,
+
+      direction: 'outgoing',
+      sender: 'user',
+    }
+    try {
+      const response = await processMessageToChatGPT([newMessage])
+      const content = response.choices[0]?.message?.content
+      if (content) {
+        const chatGPTResponse = {
+          message: content,
+          sender: 'ChatGPT',
+        }
+        console.log(`chatGPTResponse`, chatGPTResponse)
+        const { message } = chatGPTResponse
+
+        const splited = message.split('\n')
+
+        const quetions = splited[0].replace('Pergunta:', '')
+
+        const options = splited[1].replace('Alternativas:', '').split(';')
+
+        const answers = splited[2].replace('Resposta correta:', '')
+
+        setQuestion(quetions)
+        setAnswer(answers)
+        setOptions(options)
+      }
+    } catch (error) {
+      console.error('Error processing message:', error)
+    } finally {
+      // setIsTyping(false)
+    }
+  }
+
+  async function processMessageToChatGPT(chatMessages) {
+    const apiMessages = chatMessages.map((messageObject) => {
+      const role = messageObject.sender === 'ChatGPT' ? 'assistant' : 'user'
+      return { role, content: messageObject.message }
+    })
+
+    const apiRequestBody = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: "I'm a Student using ChatGPT for learning" },
+        ...apiMessages,
+      ],
+    }
+
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + process.env.NEXT_PUBLIC_API_KEY_CHAT_GPT,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+
+    return response.json()
+  }
+
+  useEffect(() => {
+    fetchQuestion()
+  }, [])
 
   return (
     <div className="bg-gradient-to-t from-marine-600 to-marine-500 h-screen text-mirage-50  flex-col flex md:items-center">
@@ -47,8 +125,8 @@ export default function Flash() {
             animate={
               shouldShowAnswer
                 ? {
-                  rotateY: 180,
-                }
+                    rotateY: 180,
+                  }
                 : {}
             }
           />
@@ -61,19 +139,24 @@ export default function Flash() {
                   initial={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
                 >
-                  Resposta
+                  {answer}
                 </motion.p>
               ) : (
                 <motion.p
                   layoutId="card-text"
-                  className="text-smoke-950 leading-base text-center  flex justify-center items-center"
+                  className="text-smoke-950 leading-base text-center  flex flex-col gap-3 justify-center items-center"
                   initial={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
                 >
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-                  dicta, nisi accusamus deserunt, eos delectus, culpa doloribus
-                  esse ex architecto illo aliquam soluta saepe recusandae non!
-                  Fugiat, odio unde? Nisi!
+                  <div>
+                    <p>{question}</p>
+                  </div>
+
+                  <div>
+                    {options.map((option) => (
+                      <p key={option}>{option}</p>
+                    ))}
+                  </div>
                 </motion.p>
               )}
             </AnimatePresence>
@@ -86,7 +169,10 @@ export default function Flash() {
       </div>
 
       <button
-        onClick={() => setShouldShowAnswer(true)}
+        onClick={() => {
+          setFlashCardCount((count) => count + 1)
+          setShouldShowAnswer(true)
+        }}
         className="bg-mirage-50 text-marine-500 py-8 w-full font-bold uppercase hover:bg-mirage-50/90 md:w-[320px] md:mb-16 md:rounded-full md:py-6"
       >
         Revelar respostas
---
+++ apps/web/src/openai/index.ts
@@ -0,0 +1,6 @@
+import OpenAI from 'openai'
+
+export const openai = new OpenAI({
+  organization: 'org-2qyCU1wk4d5ylfj0PPewRqiW',
+  apiKey: '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',
+})
